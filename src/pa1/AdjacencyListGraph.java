a36c9c9190327fcaba8486a78959dcb4c2620489
Grayson Cox <GraysonCox98@gmail.com>
2019-10-26 19:05:11 -0500

    I implemented the graph, I think.

---
+++ src/pa1/AdjacencyListGraph.java
@@ -0,0 +1,70 @@
+package pa1;
+
+import api.Graph;
+import api.TaggedVertex;
+import sun.security.provider.certpath.AdjacencyList;
+
+import java.util.*;
+
+public class AdjacencyListGraph<E> implements Graph<E> {
+
+    private ArrayList<E> vertices;
+    private ArrayList<LinkedList<Integer>> adjacencyLists;
+
+    public AdjacencyListGraph() {
+        vertices = new ArrayList<>();
+        adjacencyLists = new ArrayList<>();
+    }
+
+    public void addVertex(E v) {
+        vertices.add(v);
+        adjacencyLists.add(new LinkedList<>());
+    }
+
+    public void addEdge(int u, int v) {
+        if (u >= vertices.size() || v >= vertices.size()) {
+            return; // TODO: Better error handling
+        }
+        adjacencyLists.get(u).add(v);
+    }
+
+    // MARK - Graph<E>
+
+    @Override
+    public ArrayList<E> vertexData() {
+        return vertices;
+    }
+
+    @Override
+    public ArrayList<TaggedVertex<E>> vertexDataWithIncomingCounts() { // TODO: Refactor
+        ArrayList<TaggedVertex<E>> taggedVertices = new ArrayList<>();
+        int incomingCounts[] = new int[adjacencyLists.size()];
+        for (List<Integer> adjList : adjacencyLists) {
+            for (int i : adjList) {
+                incomingCounts[i]++;
+            }
+        }
+        for (int i = 0; i < adjacencyLists.size(); i++) {
+            taggedVertices.add(new TaggedVertex<E>(vertices.get(i), incomingCounts[i]));
+        }
+        return taggedVertices;
+    }
+
+    @Override
+    public List<Integer> getNeighbors(int index) {
+        return adjacencyLists.get(index);
+    }
+
+    @Override
+    public List<Integer> getIncoming(int index) {
+        List<Integer> incoming = new ArrayList<>();
+        for (int i = 0; i < adjacencyLists.size(); i++) {
+            for (int j = 0; j < adjacencyLists.get(i).size(); j++) { // TODO: Refactor, improve efficiency
+                if (adjacencyLists.get(i).get(j) == index) {
+                    incoming.add(i);
+                }
+            }
+        }
+        return incoming;
+    }
+}
---
+++ src/test/AdjacencyListGraphTest.java
@@ -0,0 +1,102 @@
+package test;
+
+import api.TaggedVertex;
+import pa1.AdjacencyListGraph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdjacencyListGraphTest {
+
+    final String testVertices[] = {
+            "A",
+            "B",
+            "C",
+            "D"
+    };
+
+    final int testEdges[][] = {
+            {0, 1},
+            {0, 3},
+            {1, 2},
+            {2, 3},
+            {3, 1}
+    };
+
+    final int testVertex = 3;
+
+    AdjacencyListGraph<String> graph = new AdjacencyListGraph();
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        for (int i = 0; i < testVertices.length; i++) {
+            graph.addVertex(testVertices[i]);
+        }
+        for (int i = 0; i < testEdges.length; i++) {
+            graph.addEdge(testEdges[i][0], testEdges[i][1]);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void addVertex() {
+        String s = "New Vertex";
+        graph.addVertex(s);
+        assert graph.vertexData().get(graph.vertexData().size() - 1) == s;
+    }
+
+    @org.junit.jupiter.api.Test
+    void addEdge() {
+        // TODO
+    }
+
+    @org.junit.jupiter.api.Test
+    void vertexData() {
+        assertArrayEquals(testVertices, graph.vertexData().toArray());
+    }
+
+    @org.junit.jupiter.api.Test
+    void vertexDataWithIncomingCounts() {
+        int[] expectedIncomingCounts = new int[testVertices.length];
+        List<TaggedVertex<String>> actualVertexDataWithIncomingCounts = graph.vertexDataWithIncomingCounts();
+        for (int i = 0; i < testVertices.length; i++) {
+            for (int[] testEdge : testEdges) {
+                if (testEdge[1] == i) {
+                    expectedIncomingCounts[i]++;
+                }
+            }
+        }
+        for (int i = 0; i < actualVertexDataWithIncomingCounts.size(); i++) {
+            assert actualVertexDataWithIncomingCounts.get(i).getTagValue() == expectedIncomingCounts[i];
+            assert actualVertexDataWithIncomingCounts.get(i).getVertexData().equals(testVertices[i]);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNeighbors() {
+        int v = testVertex;
+        List<Integer> expectedNeighbors = new ArrayList<>();
+        for (int i = 0; i < testEdges.length; i++) {
+            if (testEdges[i][0] == v) {
+                expectedNeighbors.add(testEdges[i][1]);
+            }
+        }
+        List<Integer> actualNeighbors = graph.getNeighbors(v);
+        assert actualNeighbors.equals(expectedNeighbors);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getIncoming() {
+        int v = testVertex;
+        List<Integer> expectedIncoming = new LinkedList<>();
+        for (int i = 0; i < testEdges.length; i++) {
+            if (testEdges[i][1] == v) {
+                expectedIncoming.add(testEdges[i][0]);
+            }
+        }
+        List<Integer> actualIncoming = graph.getIncoming(v);
+        assert actualIncoming.equals(expectedIncoming);
+    }
+}
